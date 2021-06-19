f632d9373eb19a52f088d7403f98bd9fe638fe78
OGA <totti0927.oga@gmail.com>
2021-06-19 10:19:58 +0900

    コンポーネント読み込み

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,16 +2,19 @@
   <section class="home">
     <HomeHero/>
     <HomeAbout/>
+    <homeWork/>
   </section>
 </template>
 <script>
 import HomeHero from './HomeHero.vue'
 import HomeAbout from './HomeAbout.vue'
+import homeWork from './HomeWork.vue'
 
 export default {
   components: {
     HomeHero,
-    HomeAbout
+    HomeAbout,
+    homeWork
   }
 }
 </script>
