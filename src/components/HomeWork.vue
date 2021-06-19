62a9275c0b88b872f6016083cb6915aa0781a7a4
OGA <totti0927.oga@gmail.com>
2021-06-19 15:43:15 +0900

    test

--- src/components/HomeWork.vue
+++ src/components/HomeWork.vue
@@ -2,6 +2,7 @@
   <section class="home-work">
     <div class="section__title">
       <div class="section__title-text">02. My Work</div>
+      <div class="section__title-text">02. My Work</div>
     </div>
   </section>
 </template>
