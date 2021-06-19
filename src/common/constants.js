0e8e32ce926f5625f49cb9f02c4c70522cf037c9
OGA <totti0927.oga@gmail.com>
2021-06-19 12:07:49 +0900

    レンダリング

--- src/common/constants.js
+++ src/common/constants.js
@@ -20,5 +20,19 @@ export default {
       name: 'Contact',
       url: 'contact'
     }
+  ],
+  workItems: [
+    {
+      src: require('../assets/img/b27a21c6cd2885a72dc862e482dfbfca.jpg'),
+      url: '/work#work-1'
+    },
+    {
+      src: require('../assets/img/acda5d68345825119ccbf6c9b7892451.jpg'),
+      url: '/work#work-2'
+    },
+    {
+      src: require('../assets/img/acda5d68345825119ccbf6c9b7892451.jpg'),
+      url: '/work#work-3'
+    }
   ]
 }
