bf83b5742b6d016267cda6aa08ee8dae9a594d4a
OGA <totti0927.oga@gmail.com>
2021-06-20 09:45:42 +0900

    homeserviceコンポーネント

---
+++ src/components/HomeService.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="home-work">
+    <div class="section__title">
+      <div class="section__title-text">03. My Service</div>
+    </div>
+    <v-cantainer>
+      <v-row class="home-service__contents">
+        <v-col md="3">
+          <v-card
+            to="/service#direction"
+            hover
+          >
+            <div class="home-service__card-title">
+              <h3>ディレクション</h3>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-cantainer>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "@/assets/sass/_parts.scss";
+
+</style>
